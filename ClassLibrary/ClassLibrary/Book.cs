7fcaf2e0394a4502ce45c8b3b6e2caeb1458e416
nathanxdlol <72072972+nathanxdlol@users.noreply.github.com>
2021-10-14 19:49:54 +0200

    Add files via upload

---
+++ ClassLibrary/ClassLibrary/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int ISBN13 { get; set; }
+
+        public Book(string title, string author, int pageNumber, int isbn13)
+        {
+            Title = title;
+            Author = author;
+            PageNumber = pageNumber;
+            ISBN13 = isbn13;
+        }
+
+    }
+}
